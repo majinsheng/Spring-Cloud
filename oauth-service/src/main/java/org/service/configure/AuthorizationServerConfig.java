f87818921a273dd2bc22a4b84fb3d23e876b8d00
Eric Ma <13636577862@139.com>
2018-06-03 08:33:49 +0800

    remove BCrypwde

--- oauth-service/src/main/java/org/service/configure/AuthorizationServerConfig.java
+++ oauth-service/src/main/java/org/service/configure/AuthorizationServerConfig.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -31,11 +30,6 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     
     @Autowired
     private DataSource dataSource; 
-	
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
     
     @Bean
     public ClientDetailsService clientDetails() {
